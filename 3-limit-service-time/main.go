a96a87a6cc6ff94326dd63546387285fde616fbc
Sudatra Ghosh <sudatraghosh@jmangroup.com>
2025-07-28 17:26:51 +0530

    added service time limit in freemium model

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,19 +10,47 @@
 
 package main
 
+import (
+	"time"
+	"sync"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	mu 		  sync.Mutex
 }
+
+const FreeTimeLimitPerUser = 10;
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	start := time.Now();
+	process();
+	duration := time.Since(start);
+	durationInSeconds := int64(duration.Seconds() + 0.5);
+
+	if u.IsPremium {
+		return true;
+	}
+
+	u.mu.Lock();
+	defer u.mu.Unlock();
+
+	if durationInSeconds > FreeTimeLimitPerUser {
+		return false;
+	}
+
+	if durationInSeconds + u.TimeUsed > FreeTimeLimitPerUser {
+		return false;
+	}
+
+	u.TimeUsed += durationInSeconds;
+	return true;
 }
 
 func main() {
